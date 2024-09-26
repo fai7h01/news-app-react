27c332499c9a02f68eb7dcb666e270e8dfdfc556
fai7h01 <l.varsimashvil10@gmail.com>
2024-09-26 20:22:25 +0400

    list of us cities

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import CityList from "./assets/components/CityList.tsx";
+
+function App() {
+    return (
+        <div>
+            <CityList />
+        </div>
+    );
+}
+
+export default App;
